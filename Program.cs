99e1ab2d466896ee87c0be293698f061c44b9ea8
Nick Zaccaro <nzaccaro@mail.usf.edu>
2020-02-04 17:43:55 -0500

    Commit all files

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ask user for name
+            Console.WriteLine("Please enter your name: ");
+
+            //create a string variable from input
+            string name = Console.ReadLine();
+            
+            //call greet user method to greet the user
+            Greet_User(name);
+        }
+
+        //method that takes input from user and greets their input
+        private static void Greet_User (string input)
+        {  
+            Console.WriteLine("Hello " + input + "!");
         }
     }
 }
