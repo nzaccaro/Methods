bd3c605548e1220e1a0b1c35c91c8cf1aac51040
Nick Zaccaro <nzaccaro@mail.usf.edu>
2020-02-04 21:51:59 -0500

    Commit changes

--- Program.cs
+++ Program.cs
@@ -11,15 +11,35 @@ namespace Methods
 
             //create a string variable from input
             string name = Console.ReadLine();
-            
+
             //call greet user method to greet the user
             Greet_User(name);
         }
 
         //method that takes input from user and greets their input
         private static void Greet_User (string input)
-        {  
-            Console.WriteLine("Hello " + input + "!");
+        {
+            int i = 0;
+            Boolean check = false;
+            while (i < input.Length)
+            {
+                if(Char.IsNumber(input, i) == true)
+                {
+                    Console.WriteLine("Your input is not within the parameters of a correct name :(");
+                    Console.WriteLine("Please press any key to leave the program.");
+                    i = input.Length;
+                    check = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            if (check == false)
+            {
+                Console.WriteLine("Hello " + input + "!");
+            }
         }
     }
 }
