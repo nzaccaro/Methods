ff1e5f40330d2417711b1f1c4333f75ce74fec1f
Nick Zaccaro <nzaccaro@mail.usf.edu>
2020-02-04 21:57:02 -0500

    Commit comment changes

--- Program.cs
+++ Program.cs
@@ -19,10 +19,16 @@ namespace Methods
         //method that takes input from user and greets their input
         private static void Greet_User (string input)
         {
+            //create integer variable for while loop count
             int i = 0;
+
+            //create boolean to later check if user input is clear of integers
             Boolean check = false;
+
+            //while loop to go through the user input string to check each character for an integer
             while (i < input.Length)
             {
+                //looks at the individual character in string to see if it returns true
                 if(Char.IsNumber(input, i) == true)
                 {
                     Console.WriteLine("Your input is not within the parameters of a correct name :(");
@@ -36,6 +42,7 @@ namespace Methods
                 }
             }
 
+            //makes sure that the greeting display isnt shown when there is invalid input
             if (check == false)
             {
                 Console.WriteLine("Hello " + input + "!");
