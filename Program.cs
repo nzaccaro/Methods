c9ff5bb77a94776c43fb0ac9eb935c98d37466eb
Nick Zaccaro <nzaccaro@mail.usf.edu>
2020-02-06 17:27:20 -0500

    Commit greetuser method changes

--- Program.cs
+++ Program.cs
@@ -24,6 +24,9 @@ namespace Methods
         //method that takes input from user and greets their input
         private static void Greet_User (string input)
         {
+            //removes white spaces from string
+            string result = input.Replace(" ", String.Empty);
+
             //create integer variable for while loop count
             int i = 0;
 
@@ -31,14 +34,14 @@ namespace Methods
             Boolean check = false;
 
             //while loop to go through the user input string to check each character for an integer
-            while (i < input.Length)
+            while (i < result.Length)
             {
                 //looks at the individual character in string to see if it returns true
-                if(Char.IsNumber(input, i) == true)
+                if(Char.IsLetter(result, i) == false)
                 {
                     Console.WriteLine("Your input is not within the parameters of a correct name :(");
                     Console.WriteLine("Please press any key to leave the program.");
-                    i = input.Length;
+                    i = result.Length;
                     check = true;
                 }
                 else
